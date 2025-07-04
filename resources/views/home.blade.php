d0bca9bd37de66c979408902916fdb230468d566
macrew <bitbucket@macrew.net>
2025-07-04 16:18:31 +0530

    change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-hello 
+hello Macrew !!
                     {{ __('You are logged in!') }}
                 </div>
             </div>
