2310a58111529fef7f1249d7db8cc41dd9ccc5ba
macrew <bitbucket@macrew.net>
2025-09-03 17:43:08 +0530

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
-hlooo
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_resssssssssssssplace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
